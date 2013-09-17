616522cefd8568cd6198e089e029e5741622db73
Shane Osbourne <shane.osbourne8@gmail.com>
2013-09-17 08:09:25 +0100

    Move directives into separate files

---
+++ app/scripts/controllers/draggableCtrl.js
@@ -0,0 +1,32 @@
+angular.module('ngRegexApp').controller('DraggleCtrl', ['$scope', '$rootScope', 'MethodService', 'DataStore',  function ($scope, $rootScope, MethodService, DataStore) {
+
+    $scope.data = DataStore;
+    var $sortable = jQuery(".sortable");
+
+    var updateSortOrder = function () {
+
+        $scope.$apply(function () {
+            var order = [];
+            $sortable.find('li').each(function (i, item) {
+                var order = jQuery(this).attr('rel');
+                angular.forEach($scope.data.regexSections, function (item) {
+                    if (item.$$hashKey === order) {
+                        item.sortOrder = i;
+                    }
+                });
+            });
+
+            DataStore.js_code = MethodService.renderJsCode($scope.data.regexSections);
+            DataStore.regex = MethodService.renderRegexCode($scope, DataStore.js_code);
+
+            MethodService.runTest(DataStore, DataStore.testString);
+        });
+
+    };
+
+    $scope.$on('addSortable', function () {
+        $sortable.sortable('destroy');
+        $sortable.off('sortupdate');
+        $sortable.sortable().bind('sortupdate', updateSortOrder);
+    });
+}]);
---
+++ app/scripts/directives/onFinishRender.js
@@ -0,0 +1,12 @@
+angular.module('ngRegexApp').directive('onFinishRender', ['$timeout', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit(attr.onFinishRender);
+                });
+            }
+        }
+    }
+}]);
